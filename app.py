b6f15f0461073893d914acdc3aba5def3d9e1d03
abhishek <abhishekmondal065@gmail.com>
2024-12-14 10:02:32 +0530

    style update

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask , render_template
 
 
 app = Flask(__name__)
+# print(app)
 @app.route("/") ##set the route
 
 def hello_world():
-    return render_template("Home.html")
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
