fe244ea4a21d2574ba6ec880999f9a711df0e741
abhishek <abhishekmondal065@gmail.com>
2024-12-14 21:06:29 +0530

    modifiy nav & carsel

--- app.py
+++ app.py
@@ -10,3 +10,7 @@ def hello_world():
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
+    
+# def text():
+#      x =  "Naturaly Beautiful Makeup For Your Spacial Event"
+#     return x
